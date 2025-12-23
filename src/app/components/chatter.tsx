da4cbcc5157298021811b02b9ff559c9624f1ec8
kaspar.bachmann <kaspar.bachmann@santismail.ch>
2025-12-23 09:07:52 +0100

    Comment out alert and setOpenProfile calls in Chatter component

--- src/app/components/chatter.tsx
+++ src/app/components/chatter.tsx
@@ -95,7 +95,7 @@ export default function Chatter() {
 
     setAllImages(allImages);
 
-    alert(allFirstImages[0].imageBase64);
+    //alert(allFirstImages[0].imageBase64);
 
     const validImages = allFirstImages.map((img) =>
       Boolean(img) ? img : null
@@ -352,7 +352,7 @@ export default function Chatter() {
     //window.location.href = `/home`;
     setopenChat(false);
     setSelectedProfileIndex(index);
-    setOpenProfile(true);
+    //setOpenProfile(true);
   }
 
   return (
@@ -512,9 +512,7 @@ export default function Chatter() {
               className="size-6 text-yellow-600"
             >
               <path
-                fill-rule="evenodd"
                 d="M12 1.5a.75.75 0 0 1 .75.75V4.5a.75.75 0 0 1-1.5 0V2.25A.75.75 0 0 1 12 1.5ZM5.636 4.136a.75.75 0 0 1 1.06 0l1.592 1.591a.75.75 0 0 1-1.061 1.06l-1.591-1.59a.75.75 0 0 1 0-1.061Zm12.728 0a.75.75 0 0 1 0 1.06l-1.591 1.592a.75.75 0 0 1-1.06-1.061l1.59-1.591a.75.75 0 0 1 1.061 0Zm-6.816 4.496a.75.75 0 0 1 .82.311l5.228 7.917a.75.75 0 0 1-.777 1.148l-2.097-.43 1.045 3.9a.75.75 0 0 1-1.45.388l-1.044-3.899-1.601 1.42a.75.75 0 0 1-1.247-.606l.569-9.47a.75.75 0 0 1 .554-.68ZM3 10.5a.75.75 0 0 1 .75-.75H6a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 10.5Zm14.25 0a.75.75 0 0 1 .75-.75h2.25a.75.75 0 0 1 0 1.5H18a.75.75 0 0 1-.75-.75Zm-8.962 3.712a.75.75 0 0 1 0 1.061l-1.591 1.591a.75.75 0 1 1-1.061-1.06l1.591-1.592a.75.75 0 0 1 1.06 0Z"
-                clip-rule="evenodd"
               />
             </svg>
             </div>
