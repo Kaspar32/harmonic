34106542bbb9c83a2595699bfab26bbc5dfb7e7b
kaspar.bachmann <kaspar.bachmann@santismail.ch>
2026-06-16 10:17:13 +0200

    dynamische route

--- src/app/images/[...name]/route.ts
+++ src/app/images/[...name]/route.ts
@@ -1,35 +1,83 @@
 // app/images/[...name]/route.ts
 import { join } from "path";
 import { promises as fs } from "fs";
-
+import { and, eq, sql } from "drizzle-orm";
+import { likes, users, } from "@/db/schema";
+import { db } from "@/db";
+ 
 export async function GET(
   request: Request,
   { params }: { params: Promise<{ name: string[] }> },
 ) {
-
+ 
   //console.log("Teest into dynamische route"+userId);
-
+ 
   try {
-
-    const url = new URL(request.url);
-    const blurFlag = url.searchParams.get("blur");
-
+ 
     // Warte auf das gesamte params-Objekt
-
+ 
     const awaitedParams = await Promise.resolve(params || {});
     const fileSegments = awaitedParams.name || [];
-
-
-    const filePath = join(process.cwd(), "uploads", "images", ...fileSegments);
-
-    const isBlurred = filePath.endsWith("_blurred.png");
-
-    if (isBlurred && !blurFlag) {
-      return new Response(null, { status: 404 });
+ 
+    let userfromAuth;
+    try {
+      const res = await fetch("http://localhost:3000/api/auth/me", {
+        method: "GET",
+        headers: {
+          cookie: request.headers.get("cookie") ?? "",
+        },
+      });
+ 
+      if (res.ok) {
+        const UserData = await res.json();
+        userfromAuth = UserData;
+      }
+    } catch (err) {
+      console.error("Error fetching user:", err);
     }
-
+ 
+    const currentUser = userfromAuth.uuid;
+ 
+    const targetUser = await db
+      .select()
+      .from(users)
+      .where(
+        sql`${users.profile_pics}::text LIKE ${"%" + fileSegments + "%"}`
+      );
+ 
+    const ilikedRes = await db
+      .select()
+      .from(likes)
+      .where(
+        and(
+          eq(likes.from, currentUser),
+          eq(likes.to, targetUser[0].uuid)
+        )
+      );
+ 
+    const iliked = ilikedRes.length > 0;
+ 
+    const targetLikedRes = await db
+      .select()
+      .from(likes)
+      .where(
+        and(
+          eq(likes.from, targetUser[0].uuid),
+          eq(likes.to, currentUser)
+        )
+      );
+ 
+    const targetLiked = targetLikedRes.length > 0;
+ 
+    let filePath = join(process.cwd(), "uploads", "images", ...fileSegments);
+ 
     // Dateityp-Erkennung
     const ext = (filePath.split(".").pop() ?? "").toLowerCase();
+ 
+    if (targetLiked && !iliked) {
+      filePath = filePath.replace("." + ext, "_blurred." + ext);
+    }
+ 
     const contentType =
       {
         png: "image/png",
@@ -39,9 +87,9 @@ export async function GET(
         webp: "image/webp",
         svg: "image/svg+xml",
       }[ext] || "application/octet-stream";
-
+ 
     let fileBuffer = await fs.readFile(filePath);
-
+ 
     // Convert Buffer to Uint8Array for Response compatibility
     return new Response(new Uint8Array(fileBuffer), {
       status: 200,
